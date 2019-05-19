f45e6b89182bb33cbab4be1065a8b1f3a3192f1d
purplebubblegum <muhammaddetaaditya@gmail.com>
2019-05-19 12:32:04 +0700

    Added icons

---
+++ src/components/atoms/Icon/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+function Icon(props) {
+	return <FontAwesomeIcon icon={props.icon} />
+}
+
+export default Icon
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store';
 
+import './icons';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';

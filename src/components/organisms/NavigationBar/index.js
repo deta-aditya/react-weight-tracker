72cb044090c60bad52593963daa388cb35884269
purplebubblegum <muhammaddetaaditya@gmail.com>
2019-05-19 13:49:46 +0700

    Fixed style on navbar so it is fixed

--- src/components/organisms/NavigationBar/index.js
+++ src/components/organisms/NavigationBar/index.js
@@ -5,12 +5,14 @@ import './style.css'
 function NavigationBar(props) {
 	return (
 		<nav className="NavigationBar">
-			<h1 className="brand">
-				Weight Tracker
-			</h1>
-			<div className="navigation-buttons">
-				<button>Add Weight</button>
-				<AnchoredIcon icon="sign-out-alt" size="lg" />
+			<div className="navigation-container">
+				<h1 className="brand">
+					Weight Tracker
+				</h1>
+				<div className="navigation-buttons">
+					<button>Add Weight</button>
+					<AnchoredIcon icon="sign-out-alt" size="lg" />
+				</div>
 			</div>
 		</nav>
 	)
--- src/components/pages/MainPage/index.js
+++ src/components/pages/MainPage/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { unsetEntry } from '../../../actions'
 import NavigationBar from '../../organisms/NavigationBar'
+import './style.css'
 
 function MainPage(props) {
 	return (

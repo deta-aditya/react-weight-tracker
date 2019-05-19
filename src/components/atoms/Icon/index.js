298a423caa3566b9014a784c79704954fc738617
purplebubblegum <muhammaddetaaditya@gmail.com>
2019-05-19 13:14:27 +0700

    Added size prop to Icon

--- src/components/atoms/Icon/index.js
+++ src/components/atoms/Icon/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function Icon(props) {
-	return <FontAwesomeIcon icon={props.icon} />
+	return <FontAwesomeIcon 
+		icon={props.icon}
+		size={props.size} />
 }
 
 export default Icon

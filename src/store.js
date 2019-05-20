5a173fe136fb7b3d4e6abde5cb78400b2af67ead
purplebubblegum <muhammaddetaaditya@gmail.com>
2019-05-20 19:42:40 +0700

    Set fake initial states

--- src/store.js
+++ src/store.js
@@ -4,7 +4,34 @@ import thunk from 'redux-thunk'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default function configureStore(initialState = {}) {
+// For main page development purpose we add some initial states here
+// Delete these after finished
+const initialState = {
+	entries: [
+		{
+			name: 'Kim Sejeong',
+			weights: [
+				{
+					quantity: 45,
+					takenAt: Date.now(),
+				},
+			],
+		}, 
+	],
+	selectedEntry: {
+		name: 'Kim Sejeong',
+		weights: [
+			{
+				quantity: 45,
+				takenAt: Date.now(),
+			},
+		],
+	},
+}
+
+// This is modified too! Please refer to master branch to see
+// the original function initialization
+export default function configureStore() {
 	return createStore(
 		reducers,
 		initialState,

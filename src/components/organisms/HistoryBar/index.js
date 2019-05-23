f1b60b1d0415f2dc107847400b029e915a8f4220
purplebubblegum <muhammaddetaaditya@gmail.com>
2019-05-23 17:30:37 +0700

    Added filter on history bar

--- src/components/organisms/HistoryBar/index.js
+++ src/components/organisms/HistoryBar/index.js
@@ -1,14 +1,32 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { setHistoryFilter } from '../../../actions'
 import HistoryItem from '../../molecules/HistoryItem'
 import './style.css'
 
+// Utility function
+function formatDateForDisplay(date) {
+	const boxedDate = new Date(date)
+	return `${boxedDate.getDate()}/${boxedDate.getMonth() + 1}/${boxedDate.getFullYear()}`
+}
+
 function HistoryBar(props) {
+	const displayedWeights = props.weights.filter(weight => {
+		return weight.quantity.toString().includes(props.filter)
+			|| formatDateForDisplay(weight.takenAt).includes(props.filter)
+	})
+
 	return (
 		<aside className="HistoryBar">
 			<h3 className="title">History</h3>
+			<input 
+				className="filter" 
+				type="text" 
+				placeholder="Filter..." 
+				onChange={e => props.setFilter(e.target.value)} />
 			<div className="history-list">
 				{
-					props.weights.map((weight, index) => {
+					displayedWeights.map((weight, index) => {
 						return (
 							<HistoryItem 
 								weight={weight} 
@@ -21,4 +39,15 @@ function HistoryBar(props) {
 	)
 }
 
-export default HistoryBar
+const mapStateToProps = state => ({
+	filter: state.historyFilter,
+})
+
+const mapDispatchToProps = dispatch => ({
+	setFilter: query => dispatch(setHistoryFilter(query)),
+})
+
+export default connect(
+	mapStateToProps, 
+	mapDispatchToProps
+)(HistoryBar)

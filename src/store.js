afa9dca93092193d2a6db2bf0bb56ab2b2d2fb5a
purplebubblegum <muhammaddetaaditya@gmail.com>
2019-05-26 18:53:07 +0700

    Restored store

--- src/store.js
+++ src/store.js
@@ -4,42 +4,7 @@ import thunk from 'redux-thunk'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// For main page development purpose we add some initial states here
-// Delete these after finished
-const initialState = {
-	entries: [
-		{
-			name: 'Kim Sejeong',
-			weights: [
-				{
-					quantity: 43,
-					takenAt: Date.now() + 1,
-				},
-				{
-					quantity: 45,
-					takenAt: Date.now(),
-				},
-			],
-		}, 
-	],
-	selectedEntry: {
-		name: 'Kim Sejeong',
-		weights: [
-			{
-				quantity: 43,
-				takenAt: Date.now() + 1,
-			},
-			{
-				quantity: 45,
-				takenAt: Date.now(),
-			},
-		],
-	},
-}
-
-// This is modified too! Please refer to master branch to see
-// the original function initialization
-export default function configureStore() {
+export default function configureStore(initialState = {}) {
 	return createStore(
 		reducers,
 		initialState,
